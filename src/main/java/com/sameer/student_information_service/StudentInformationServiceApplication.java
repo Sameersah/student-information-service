0d97f86f6b913c658bbfc7b967a6fa1e2335f699
Fnu Sameer <sameersah7365@gmail.com>
2024-10-15 19:26:30 -0700

    initial commit

---
+++ src/main/java/com/sameer/student_information_service/StudentInformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sameer.student_information_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInformationServiceApplication.class, args);
+	}
+
+}
